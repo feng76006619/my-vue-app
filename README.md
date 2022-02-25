aa902d09642b41487929c93c6e1cb7a59719347a
feng76006619 <93699630+feng76006619@users.noreply.github.com>
2022-02-26 04:19:34 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,24 +1,50 @@
-# vue-app
+# pixivic-refactor
+
+## schedule
+
+### Refactor
+
+- [x] 基础架构
+- [x] Home
+- [x] DailyRank
+- [x] PopSearch
+- [x] Intro
+- [x] Links
+- [x] Donate
+- [x] Comment
+- [x] User System
+- [x] Artist
+- [x] Collect
+- [x] Artist New
+- [x] Find
+
+### Optimization
+
+- [x] 搜索提示词
+- [x] 搜索的日期范围选定
+- [x] 日排行页面和搜索页面的作品详情
+- [x] 排行模式切换以及前后切换 包含左右滑动
 
 ## Project setup
 ```
-yarn install
+npm install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-yarn serve
+npm run serve
 ```
 
 ### Compiles and minifies for production
 ```
-yarn build
+npm run build
 ```
 
 ### Lints and fixes files
 ```
-yarn lint
+npm run lint
 ```
+
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue.config.js
@@ -0,0 +1,57 @@
+/*
+ * @Author: Kim
+ * @since: 2020-03-21 14:10:00
+ * @lastTime: 2020-03-25 23:01:46
+ * @LastAuthor: Dongzy
+ * @FilePath: \pixivic-mobile\vue.config.js
+ * @message:
+ */
+const path = require('path');
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+console.log('Url prefix: ', process.env.VUE_APP_PREFIX);
+
+module.exports = {
+  runtimeCompiler: true,
+  lintOnSave: false,
+  publicPath: process.env.VUE_APP_PREFIX,
+  // publicPath: './',
+
+  // 路径别名
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('static', resolve('static'));
+  },
+
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true
+      }
+    }
+  },
+
+  configureWebpack: config => {
+    const devPlugins = [new BundleAnalyzerPlugin()];
+    const plugins = [];
+    config.externals = {
+      // vue: 'Vue',
+      // 'vue-router': 'VueRouter',
+      // 'vuetify': 'Vuetify'
+      // 'element-ui': 'ElementUI'
+      // 其他三方库 ...
+    };
+    if (process.env.NODE_ENV !== 'development') {
+      config.plugins = [...config.plugins, ...plugins];
+    } else {
+      config.plugins = [...config.plugins, ...devPlugins];
+    }
+  }
+
+}
+;
